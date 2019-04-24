61aa682aff612bc0c1dceded73155aa465f09221
andikaarif <andikapradanaarif@gmail.com>
2019-04-24 07:04:32 +0700

    Start project

---
+++ src/view/dashboard/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../layout'
+import './style.css'
+
+const Login = () =>{
+	return(
+		<Layout>
+			<div className='dashboard_wrapper'>
+				tes
+			</div>
+		</Layout>
+		)
+}
+
+export default (Login)
